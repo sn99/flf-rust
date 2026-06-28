4a65a1560996180682af5d364221fa535ae65ae6
sn99 <siddharthn.099@gmail.com>
2026-06-29 03:15:04 +0530

    Deploy parity combat/AI/manager pass

---
+++ rust/flf.d.ts
@@ -0,0 +1,59 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Boot the game. `asset_root` is the path to LF2_19 assets (e.g. "assets/LF2_19").
+ */
+export function start_game(asset_root: string): Promise<void>;
+
+export function version(): string;
+
+export function wasm_start(): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly start_game: (a: number, b: number) => any;
+    readonly version: () => [number, number];
+    readonly wasm_start: () => void;
+    readonly wasm_bindgen__convert__closures_____invoke__hc95119054ba6fe56: (a: number, b: number, c: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h21a0648bc28e0ade: (a: number, b: number, c: any) => [number, number];
+    readonly wasm_bindgen__convert__closures_____invoke__h2020ed747a1b5a9e: (a: number, b: number, c: any, d: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h2e47dfbba379241d: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h2e47dfbba379241d_3: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h2e47dfbba379241d_4: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h2e47dfbba379241d_5: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h997d4b3b40b06012: (a: number, b: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_destroy_closure: (a: number, b: number) => void;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ rust/flf_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start_game: (a: number, b: number) => any;
+export const version: () => [number, number];
+export const wasm_start: () => void;
+export const wasm_bindgen__convert__closures_____invoke__hc95119054ba6fe56: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h21a0648bc28e0ade: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h2020ed747a1b5a9e: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h2e47dfbba379241d: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h2e47dfbba379241d_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h2e47dfbba379241d_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h2e47dfbba379241d_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h997d4b3b40b06012: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
